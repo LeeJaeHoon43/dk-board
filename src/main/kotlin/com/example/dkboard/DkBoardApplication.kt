ee0dd58af0128018456242b4d5d71701c3b14850
wally <wally@delivered.co.kr>
2024-02-04 23:26:29 +0900

    feat: Add first commit

---
+++ src/main/kotlin/com/example/dkboard/DkBoardApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dkboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DkBoardApplication
+
+fun main(args: Array<String>) {
+    runApplication<DkBoardApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/dkboard/DkBoardApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.example.dkboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DkBoardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
