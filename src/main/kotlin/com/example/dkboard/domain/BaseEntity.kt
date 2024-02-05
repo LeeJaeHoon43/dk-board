ca653d5e20b3d38491038384190fdb93d03038c6
wally <wally@delivered.co.kr>
2024-02-05 12:03:52 +0900

    feat: Design API and entity

---
+++ src/main/kotlin/com/example/dkboard/domain/BaseEntity.kt
@@ -0,0 +1,20 @@
+package com.example.dkboard.domain
+
+import jakarta.persistence.MappedSuperclass
+import java.time.LocalDateTime
+@MappedSuperclass
+abstract class BaseEntity(
+    createdBy: String,
+) {
+    val createdBy: String = createdBy
+    val createdAt: LocalDateTime = LocalDateTime.now()
+    var updatedBy: String? = null
+        protected set
+    var updatedAt: LocalDateTime? = null
+        protected set
+
+    fun update(updateBy: String) {
+        this.updatedBy = updateBy
+        this.updatedAt = LocalDateTime.now()
+    }
+}
