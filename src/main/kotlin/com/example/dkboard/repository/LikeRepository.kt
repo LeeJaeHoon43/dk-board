80ab231d8ec5c0b75fdb9af18ace593228d2856c
wally <wally@delivered.co.kr>
2024-02-11 17:54:49 +0900

    feat: Add count functions for likes

--- src/main/kotlin/com/example/dkboard/repository/LikeRepository.kt
+++ src/main/kotlin/com/example/dkboard/repository/LikeRepository.kt
@@ -3,4 +3,6 @@ package com.example.dkboard.repository
 import com.example.dkboard.domain.Like
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface LikeRepository : JpaRepository<Like, Long>
+interface LikeRepository : JpaRepository<Like, Long> {
+    fun countByPostId(postId: Long): Long
+}
--- src/test/kotlin/com/example/dkboard/service/PostServiceTest.kt
+++ src/test/kotlin/com/example/dkboard/service/PostServiceTest.kt
@@ -25,6 +25,7 @@ import org.springframework.data.repository.findByIdOrNull
 @SpringBootTest
 class PostServiceTest(
     private val postService: PostService,
+    private val likeService: LikeService,
     private val postRepository: PostRepository,
     private val commentRepository: CommentRepository,
     private val tagRepository: TagRepository,
@@ -187,6 +188,9 @@ class PostServiceTest(
                 Tag(name = "tag3", post = saved, createdBy = "wally")
             )
         )
+        likeService.createLike(saved.id, "wally")
+        likeService.createLike(saved.id, "wally1")
+        likeService.createLike(saved.id, "wally2")
         When("정상 조회 시") {
             val post = postService.getPost(saved.id)
             then("게시글의 내용이 정상적으로 변환됨을 확인한다.") {
@@ -200,6 +204,9 @@ class PostServiceTest(
                 post.tags[0] shouldBe "tag1"
                 post.tags[1] shouldBe "tag2"
                 post.tags[2] shouldBe "tag3"
+            }
+            then("좋아요 개수가 조회됨을 확인한다.") {
+                post.likeCount shouldBe 3
             }
         }
         When("게시글이 없을 때") {
@@ -271,6 +278,19 @@ class PostServiceTest(
                 postPage.content[2].title shouldBe "title8"
                 postPage.content[3].title shouldBe "title7"
                 postPage.content[4].title shouldBe "title6"
+            }
+        }
+        When("좋아요가 추가되었을 때") {
+            val postPage = postService.findPageBy(PageRequest.of(0, 5), PostSearchRequestDto(tag = "tag5"))
+            postPage.content.forEach {
+                likeService.createLike(it.id, "wally1")
+                likeService.createLike(it.id, "wally2")
+            }
+            val likedPostPage = postService.findPageBy(PageRequest.of(0, 5), PostSearchRequestDto(tag = "tag5"))
+            then("좋아요 개수가 정상적으로 조회됨을 확인한다.") {
+                likedPostPage.content.forEach {
+                    it.likeCount shouldBe 2
+                }
             }
         }
     }
